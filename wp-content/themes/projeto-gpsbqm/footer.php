ab6e796308594a8eb4b648211480eb0b95b8e885
beatriz <128046453+abeazist@users.noreply.github.com>
2024-12-12 00:20:05 -0300

    limpando

--- wp-content/themes/projeto-gpsbqm/footer.php
+++ wp-content/themes/projeto-gpsbqm/footer.php
@@ -1,9 +1,6 @@
 <footer>
-        <div class="rodape">
-            <a href="https://github.com/abeazist" target="_blank" >GitHub<img src="" alt=""></a>
-        </div>
-        <p>wordpress</p>
-    </footer>
+        
+</footer>
 
 </body>
 </html>
--- wp-content/themes/projeto-gpsbqm/index.php
+++ wp-content/themes/projeto-gpsbqm/index.php
@@ -1,27 +1,6 @@
 <?php get_header(); ?>
         <main background= "fundoindex.jpg">
             
-            <div class="area-container">
-            <div class="inicio">
-                <h1 class="titulo">Grupo de Pesquisa em Substâncias Bioativas e Química dos Materiais</h1>
-                <p class="introducao">O Grupo de Pesquisa em Substâncias Bioativas e 
-Química dos Materiais (GPSBQM) conta com a participação de estudantes e docentes da UTFPR de 
-Campo Mourão, acompanhe em seus projetos em seus estudos.</p>
-                <div class="conhecer">Venha conhecer!</div>
-                <p>beatriz</p>
-            </div>
-            <div class="projetos">
-                <h2>Projetos</h2>
-
-            </div>
-            <div class="equipe">
-                <h2>Equipe</h2>
-                
-
-            </div>
-            <div class="fotos">
-                <h2>Fotos</h2>
-
-            </div>
+            
         </main>
 <?php get_footer(); ?>
