f4c50b66205673ea989a6b7462c23d3087d75912
beatriz <128046453+abeazist@users.noreply.github.com>
2024-12-24 15:23:44 -0300

    transferindo

--- wp-content/themes/projeto-gpsbqm/footer.php
+++ wp-content/themes/projeto-gpsbqm/footer.php
@@ -1,6 +1,21 @@
 <footer>
-        
+    <div class="footer-left">
+        <a href="https://github.com/abeazist">
+          <img class="imgfooter" src="/img/github.svg" alt="GitHub Logo" class="icon">
+        </a>
+        <span>© copyright by abeazist from GitHub</span>
+      </div>
+      <div class="footer-right">
+        <a href="*" target="_blank">
+          <img class="imgfooter" src="/img/youtube.svg" alt="YouTube Logo" class="icon">
+        </a>
+        <a href="*" target="_blank">
+          <img class="imgfooter" src="/img/instagram.svg" alt="Instagram Logo" class="icon">
+        </a>
+    </div>
 </footer>
+
+<?php wp_footer();?> 
 
 </body>
 </html>
