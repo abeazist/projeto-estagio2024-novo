fa29a4afceb11b034d25b961c0be41cef4d1e1d5
beatriz <128046453+abeazist@users.noreply.github.com>
2025-02-24 14:45:20 -0300

    arrumando páginas

--- wp-content/themes/projeto-gpsbqm/index.php
+++ wp-content/themes/projeto-gpsbqm/index.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?>
-
-<?php get_footer();?>
+    <?php 
+        if (have_posts()):
+            while (have_posts()): the_post(); ?>
+                <?php the_content(); ?>
+            <?php endwhile; ?>
+        <?php endif;  ?>
+<?php get_footer(); ?>
